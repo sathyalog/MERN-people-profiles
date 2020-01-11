096942484ff938252b01ccbd3cec76cbd2a856de
sathya <sathya.vakacharla@gmail.com>
2020-01-11 22:14:12 +0530

    user fields validation in server

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,20 @@
 const express = require('express');
-
+const {check, validationResult } = require('express-validator');
 const router = express.Router();
 
 // @route     GET api/users
 // @desc      Users route
 // @access    Public
 
-router.get('/', (req, res) => {
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'please enter a password with 6 or more characters').isLength({min: 6})
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
     res.send('Users Route')
 })
 
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect database
 connectDB();
 
+//Init middleware
+app.use(express.json({extended:false}));
+
 app.get('/', (req,res) => {
     res.send('API running')
 })
