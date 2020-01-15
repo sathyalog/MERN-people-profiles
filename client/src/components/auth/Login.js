dcc1ad6ae49d63febc7e42986ca25cd7f886173c
sathya <sathya.vakacharla@gmail.com>
2020-01-15 23:28:19 +0530

    react login form added

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password:'',
+    });
+
+    const { email, password} = formData;
+
+    const onChangeHandler = (e) => setFormData({...formData, [e.target.name]: e.target.value});
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        console.log('Success');
+    }
+
     return (
-        <div>
-            Login
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sing Into Your Account</p>
+            <form className="form" onSubmit={e => onSubmitHandler(e)}>
+                <div className="form-group">
+                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChangeHandler(e)} required/>
+                
+                </div>
+                <div className="form-group">
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={password} 
+                    onChange={e => onChangeHandler(e)} 
+                    required
+                    minLength="6"
+                />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </Fragment>
     )
 }
 
 export default Login;
-
